fbc6ce5d68d6ff9f88355c2def370a1a07a8bd9d
Oleksandr Pikozh <o.pikozh@gmail.com>
2017-09-19 22:13:11 +0300

    When clicking on an expandable, auto-collapse other expandables.

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
 Array.prototype.forEach.call(document.querySelectorAll('.expandable'), function(expandable) {
-	expandable.querySelector('.expander').addEventListener('click', function(e) {expandable.classList.toggle('expanded');});
+	expandable.querySelector('.expander').addEventListener('click', function(e) {
+		document.querySelectorAll('.expandable').forEach(function(otherExpandable) {
+			if (otherExpandable !== expandable)
+				otherExpandable.classList.remove('expanded');
+		});
+		expandable.classList.toggle('expanded');
+	});
 });
 
 Array.prototype.forEach.call(document.querySelectorAll('[data-command]'), function(link) {
