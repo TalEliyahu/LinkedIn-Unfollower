2652bc22a3b4bb39684a2e525aa9ad25e9a8835d
Oleksandr Pikozh <o.pikozh@gmail.com>
2017-09-19 11:19:12 +0300

    Rewritten almost everything; added unfollow with filter, ignore invitations and withdraw invitations.

--- background.js
+++ background.js
@@ -1,45 +1,77 @@
-/* LISTNER */
-chrome.extension.onMessage.addListener(function(request,sender,sendResponse) {
-	if(request.msg === "UnfollowPeople") {
-		chrome.tabs.query({
-			active: true,
-			currentWindow: true
-		}, function(tabs) {
-			var ProcessingTab = tabs[0].id;
-			/*chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-				if(tabId == ProcessingTab && changeInfo.url) {
-					chrome.tabs.executeScript(ProcessingTab, {
-						file: "unfollow.js"
-					});
-				}
-			});*/
-			chrome.tabs.update(ProcessingTab, {url: "https://www.linkedin.com/feed/following/"});
-			setTimeout(function(){
-				chrome.tabs.executeScript(ProcessingTab, {
-					file: "unfollow.js"
+var actionsByMsgs = {
+	unfollowAll: {
+		url: 'https://www.linkedin.com/feed/following/',
+		code: 'unfollow(null);',
+	},
+	unfollowConnections: {
+		url: 'https://www.linkedin.com/feed/following/',
+		code: 'unfollow("filter_connections");',
+	},
+	unfollowOutOfNetwork: {
+		url: 'https://www.linkedin.com/feed/following/',
+		code: 'unfollow("filter_out_of_network");',
+	},
+	unfollowCompanies: {
+		url: 'https://www.linkedin.com/feed/following/',
+		code: 'unfollow("filter_companies");',
+	},
+	unfollowFollowers: {
+		url: 'https://www.linkedin.com/feed/followers/',
+		code: 'unfollow(null);',
+	},
+	ignoreInvitations: {
+		url: 'https://www.linkedin.com/mynetwork/invitation-manager/?filterCriteria=',
+		code: 'ignoreInvitations();',
+	},
+	ignoreInvitationsWithCommonCompany: {
+		url: 'https://www.linkedin.com/mynetwork/invitation-manager/?filterCriteria=COMMON_COMPANY',
+		code: 'ignoreInvitations();',
+	},
+	ignoreInvitationsWithCommonEducation: {
+		url: 'https://www.linkedin.com/mynetwork/invitation-manager/?filterCriteria=COMMON_EDUCATION',
+		code: 'ignoreInvitations();',
+	},
+	ignoreInvitationsWithCommonConnection: {
+		url: 'https://www.linkedin.com/mynetwork/invitation-manager/?filterCriteria=COMMON_CONNECTION',
+		code: 'ignoreInvitations();',
+	},
+	withdrawSentInvitations: {
+		url: 'https://www.linkedin.com/mynetwork/invitation-manager/sent/',
+		code: 'withdraw();',
+	},
+};
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+	if (sender.id !== chrome.runtime.id)
+		return;
+
+	var action = actionsByMsgs[request.msg];
+	if (!action)
+		return console.log('Unexpected request message: "' + request.msg + '"');
+		
+	chrome.tabs.query({
+		active: true,
+		currentWindow: true
+	}, function(tabs) {
+		chrome.tabs.update(tabs[0].id, {url: action.url}, function(updatedTab) {
+			function injectScript() {
+				chrome.tabs.executeScript(
+					updatedTab.id, 
+					{file: 'unfollow.js'}, 
+					function(executedScriptResults) {
+						chrome.tabs.executeScript(updatedTab.id, {code: action.code});
+					}
+				);
+			}
+			if (updatedTab.status === 'complete')
+				injectScript();
+			else
+				chrome.tabs.onUpdated.addListener(function listener(updatedTabId, changeInfo, tab) {
+					if (updatedTabId === updatedTab.id && changeInfo.status === 'complete') {
+						chrome.tabs.onUpdated.removeListener(listener);
+						injectScript();
+					}
 				});
-			}, 7500);
 		});
-	}
-	if(request.msg === "UnfollowFollowers") {
-		chrome.tabs.query({
-			active: true,
-			currentWindow: true
-		}, function(tabs) {
-			var ProcessingTab = tabs[0].id;
-			/*chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-				if(tabId == ProcessingTab && changeInfo.url) {
-					chrome.tabs.executeScript(ProcessingTab, {
-						file: "unfollow.js"
-					});
-				}
-			});*/
-			chrome.tabs.update(ProcessingTab, {url: "https://www.linkedin.com/feed/followers/"});
-			setTimeout(function(){
-				chrome.tabs.executeScript(ProcessingTab, {
-					file: "unfollow.js"
-				});
-			}, 7500);
-		});
-	}
+	});
 });
--- unfollow.js
+++ unfollow.js
@@ -1,63 +1,327 @@
-function isHidden(el) {
-	if(el) return (el.offsetParent === null);
-	else return false;
-}
+var config = {
+	smoothUnfollow: false,
+	withdrawMinimalPause: 0, //milliseconds for asynchronous, or false for synchronous
+	imitate: false,
+};
 
-TotalUnfollowed = 0;
+(function() {
+	function OurForm(confirmationText) {
+		var ourForm = this;
+		
+		var rootDiv = document.createElement('div');
+		rootDiv.id = 'LiUnfollow';
+		rootDiv.style = "width: 300px; height: auto; background: #f6f7f9; z-index: 9999999999; position: fixed; top: calc(50% - 100px); right: calc(50% - 150px); border: 1px solid #ced0d4; border-radius: 2px; font-family: \'Gill Sans\', \'Gill Sans MT\', \'Myriad Pro\', \'DejaVu Sans Condensed\', Helvetica, Arial, \'sans-serif\'; font-size: medium; font-weight: normal;";
+		rootDiv.innerHTML = '<div style="color: aliceblue; background-color: #005E93; border-bottom: 1px solid #ced0d4; padding: 7.5px; text-align: center;">'
+			+ '    LinkedIn Unfollower'
+			+ '</div>'
+			+ '<div name="Confirmation">'
+			+ '    <div style="font-size: 14px; color: #4b4f56; margin: 7.5px; display: block; cursor: default;">' + confirmationText + '</div>'
+			+ '    <div style="background-color: #005E93; border-top: 1px solid #ced0d4; padding: 7.5px;">'
+			+ '        <button type="button" name="Start" '
+			+ '        style="background-color: #f6f7f9; color: #4b4f56; border: 1px solid #ced0d4; padding: 0 8px; line-height: 27.5px; font-weight: normal; font-family: \'Gill Sans\', \'Gill Sans MT\', \'Myriad Pro\', \'DejaVu Sans Condensed\', Helvetica, Arial, \'sans-serif\'; font-size: 14px;" '
+			+ '        >Yes</button>'
+			+ '        <button name="Close" '
+			+ '        style="background-color: #f6f7f9; color: #4b4f56; border: 1px solid #ced0d4; padding: 0 8px; line-height: 27.5px; font-weight: normal; font-family: \'Gill Sans\', \'Gill Sans MT\', \'Myriad Pro\', \'DejaVu Sans Condensed\', Helvetica, Arial, \'sans-serif\'; font-size: 14px; margin-left: 7.5px;" '
+			+ '        >No</button>'
+			+ '    </div>'
+			+ '</div>'
+			+ '<div name="Working" style="display: none;">'
+			+ '    <div name="Message" style="font-size: 14px; color: #4b4f56; margin: 7.5px; cursor: default; display: block;"></div>'
+			+ '    <div style="background-color: #005E93; border-top: 1px solid #ced0d4; padding: 7.5px;">'
+			+ '        <button name="Close" '
+			+ '        style="background-color: #f6f7f9; color: #4b4f56; border: 1px solid #ced0d4; padding: 0 8px; line-height: 27.5px; font-weight: normal; font-family: \'Gill Sans\', \'Gill Sans MT\', \'Myriad Pro\', \'DejaVu Sans Condensed\', Helvetica, Arial, \'sans-serif\'; font-size: 14px;" '
+			+ '        >Stop</button>'
+			+ '    </div>'
+			+ '</div>'
+			+ '<div name="Finished" style="display: none;">'
+			+ '    <div name="Message" style="font-size: 14px; color: #4b4f56; margin: 7.5px; cursor: default; display: block;"></div>'
+			+ '    <div style="background-color: #005E93; border-top: 1px solid #ced0d4; padding: 7.5px;">'
+			+ '        <button name="Close" '
+			+ '        style="background-color: #f6f7f9; color: #4b4f56; border: 1px solid #ced0d4; padding: 0 8px; line-height: 27.5px; font-weight: normal; font-family: \'Gill Sans\', \'Gill Sans MT\', \'Myriad Pro\', \'DejaVu Sans Condensed\', Helvetica, Arial, \'sans-serif\'; font-size: 14px;" '
+			+ '        >Close</button>'
+			+ '    </div>'
+			+ '</div>';
+		var confirmationDiv = rootDiv.querySelector('[name=Confirmation]');
+		var workingDiv = rootDiv.querySelector('[name=Working]');
+		var finishedDiv = rootDiv.querySelector('[name=Finished]');
 
-var LoadingAnimation = document.getElementsByClassName("initial-load-animation")[0];
-var CatchLoadingAnimation = setInterval(function(){
-	if(isHidden(LoadingAnimation) == true) {
-		clearInterval(CatchLoadingAnimation);
-		/* START: RUN AFTER PAGE LOAD COMPLETE */
-		/* SHOW STATUS AND CONFIRMATION ELEMENT */
-		var LiUnfollow = document.createElement("div");
-			LiUnfollow.id = "LiUnfollow";
-			LiUnfollow.innerHTML = "<div style=\"color: aliceblue; background-color: #005E93; border-bottom: 1px solid #ced0d4; padding: 7.5px; text-align: center;\">LinkedIn Unfollower</div><div name=\"Confirmation\"><div style=\"font-size: 14px; color: #4b4f56; margin: 7.5px; display: block; cursor: default;\">Are You Sure You Want to Unfollow All Peoples?</div><div style=\"background-color: #005E93; border-top: 1px solid #ced0d4; padding: 7.5px;\"><button type=\"button\" name=\"Continue\" style=\"background-color: #f6f7f9; color: #4b4f56; border: 1px solid #ced0d4; padding: 0 8px; line-height: 27.5px; font-weight: normal; font-family: 'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'DejaVu Sans Condensed', Helvetica, Arial, 'sans-serif'; font-size: 14px;\">Yes</button><button type=\"button\" name=\"Close\" style=\"background-color: #f6f7f9; color: #4b4f56; border: 1px solid #ced0d4; padding: 0 8px; line-height: 27.5px; font-weight: normal; font-family: 'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'DejaVu Sans Condensed', Helvetica, Arial, 'sans-serif'; font-size: 14px; margin-left: 7.5px;\">No</button></div></div><div name=\"Status\" style=\"display: none;\"><div style=\"font-size: 14px; color: #4b4f56; margin: 7.5px; display: block; cursor: default;\"><div name=\"Message\" style=\"display: none;\">Unfollowed Successfully!</div>Total Unfollowed: <span name=\"TotalUnfollowed\">0</span></div><div style=\"background-color: #005E93; border-top: 1px solid #ced0d4; padding: 7.5px;\"><button type=\"button\" name=\"Stop\" style=\"background-color: #f6f7f9; color: #4b4f56; border: 1px solid #ced0d4; padding: 0 8px; line-height: 27.5px; font-weight: normal; font-family: 'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'DejaVu Sans Condensed', Helvetica, Arial, 'sans-serif'; font-size: 14px;\">Stop</button></div></div>";
-			LiUnfollow.style = "width: 300px; height: auto; background: #f6f7f9; z-index: 9999999999; position: fixed; top: calc(50% - 100px); right: calc(50% - 150px); border: 1px solid #ced0d4; border-radius: 2px; font-family: 'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'DejaVu Sans Condensed', Helvetica, Arial, 'sans-serif'; font-size: medium; font-weight: normal;";
-		document.body.appendChild(LiUnfollow);
-
-		/* ONCLICK EVENTS */
-		/* NO BUTTON */
-		if(document.querySelectorAll("#LiUnfollow [name=Close]")[0]) document.querySelectorAll("#LiUnfollow [name=Close]")[0].onclick = function() {
-			document.querySelectorAll("#LiUnfollow")[0].outerHTML = "";
+		ourForm.onStart = function() {};
+		confirmationDiv.querySelector('[name=Start]').addEventListener('click', function(e) {
+			confirmationDiv.style.display = 'none';
+			workingDiv.style.display = 'block';
+			ourForm.onStart();
+		});
+		confirmationDiv.querySelector('[name=Close]').addEventListener('click', function(e) {close();});
+		
+		ourForm.onAbort = function() {};
+		workingDiv.querySelector('[name=Close]').addEventListener('click', function(e) {
+			ourForm.onAbort();
+			ourForm.finished();
+		});
+		
+		finishedDiv.querySelector('[name=Close]').addEventListener('click', function(e) {close();});
+		
+		ourForm.setProgressMessage = function(messageText) {
+			workingDiv.querySelector('[name=Message]').innerText = messageText;
 		};
 
-		/* YES BUTTON */
-		if(document.querySelectorAll("#LiUnfollow [name=Continue]")[0]) document.querySelectorAll("#LiUnfollow [name=Continue]")[0].onclick = function() {
-			document.querySelectorAll("#LiUnfollow [name=Confirmation]")[0].style.display = "none";
-			document.querySelectorAll("#LiUnfollow [name=Status]")[0].style.display = "block";
-			UnfollowFunction = setInterval(function() {
-				var UnfollowButton = document.querySelectorAll("ul > li [data-control-name=\"actor_follow_toggle\"].is-following");
-				for(var i = UnfollowButton.length - 1; i>=0; i--) {
-					TotalUnfollowed++;
-					UnfollowButton[i].click();
-					document.querySelectorAll("#LiUnfollow [name=TotalUnfollowed]")[0].innerHTML = Number(document.querySelectorAll("#LiUnfollow [name=TotalUnfollowed]")[0].innerHTML) + 1;
-				}
-			}, 1000);
-			ScrollToBottom = setInterval(function() {
-				window.scrollBy(0, 12.5);
-			}, 15);
-			function CheckPageEnd(LastHeight) {
-				var CurrentHeight = document.body.scrollHeight;
-				if(!(CurrentHeight > LastHeight)) {
-					clearInterval(ScrollToBottom);
-					clearInterval(UnfollowFunction);
-					document.querySelectorAll("#LiUnfollow [name=Message]")[0].style.display = "block";
-					document.querySelectorAll("#LiUnfollow [name=Stop]")[0].innerHTML = "Close";
-				} else setTimeout(function() {
-					CheckPageEnd(CurrentHeight);
-				}, 7.5*1000);
-			}
-			CheckPageEnd(0);
-		}
-
-		/* STOP BUTTON */
-		if(document.querySelectorAll("#LiUnfollow [name=Stop]")[0]) document.querySelectorAll("#LiUnfollow [name=Stop]")[0].onclick = function() {
-			clearInterval(ScrollToBottom);
-			clearInterval(UnfollowFunction);
-			document.querySelectorAll("#LiUnfollow")[0].outerHTML = "";
+		ourForm.finished = function() {
+			confirmationDiv.style.display = 'none';
+			workingDiv.style.display = 'none';
+			finishedDiv.querySelector('[name=Message]').innerText = workingDiv.querySelector('[name=Message]').innerText;
+			finishedDiv.style.display = 'block';
 		};
-		/* END: RUN AFTER PAGE LOAD COMPLETE */
+
+		ourForm.show = function() {document.body.appendChild(rootDiv);};
+		function close() {document.body.removeChild(rootDiv);}
 	}
-}, 1000);
+	
+	
+	function retryWhileFails(failableAction, interval) {
+		interval = interval || 100;
+		return (function tryDoNow() {
+			var actionResult = failableAction();
+			if (actionResult !== undefined)
+				return setTimeout(tryDoNow, actionResult !== false ? actionResult : interval);
+		})();
+	}
+	
+	function isVisible(element) {return !!element.offsetParent;}
+	
+	function doWhenLoaded(action) {
+		function doActionAfterLoadAnimation() {
+			var linkedinLoadingAnimation = document.querySelector('.initial-load-animation');
+			retryWhileFails(function() {
+				if (isVisible(linkedinLoadingAnimation))
+					return false;
+				action();
+			});
+		}
+		
+		if (document.readyState === 'complete')
+			doActionAfterLoadAnimation();
+		else
+			document.addEventListener('load', function(e) {doActionAfterLoadAnimation();});
+	}
+	
+	
+	window.unfollow = function(filterButtonControlName) {
+		doWhenLoaded(function doUnfollow() {
+			function getFollowUnfollowButtons() {return document.querySelectorAll('.feed-s-follow-recommendation-card [data-control-name="actor_follow_toggle"]');}
+			
+			if (filterButtonControlName) {
+				retryWhileFails(function() {
+					var filterListButton = document.querySelector('[data-control-name="filter_following_list"]');
+					if (!filterListButton)
+						return false;
+					filterListButton.click();
+					retryWhileFails(function() {
+						var specificFilterButton = document.querySelector('[data-control-name="' + filterButtonControlName + '"]');
+						if (!specificFilterButton)
+							return false;
+						var buttons = getFollowUnfollowButtons();
+						specificFilterButton.click();
+						retryWhileFails(function() {
+							if (Array.prototype.some.call(buttons, isVisible))
+								return false;
+							unfollow(null);
+						});
+					});
+				});
+				return;
+			}
+			
+
+			var ourForm = new OurForm('Are You Sure You Want to Unfollow Everybody On This Page?');
+			if (!config.smoothUnfollow) {
+				var totalItemsCount;
+				var processedItemsCount;
+				function updateProgressMessage() {ourForm.setProgressMessage(processedItemsCount + ' were unfollowed so far...');}
+				var loopingId;
+				
+				ourForm.onStart = function() {
+					totalItemsCount = 0;
+					processedItemsCount = 0;
+					updateProgressMessage();
+					
+					var waitingEndTime = -Infinity;
+					loopingId = retryWhileFails(function() {
+						var buttons = getFollowUnfollowButtons();
+						var time = Date.now();
+						if (totalItemsCount < buttons.length) {
+							totalItemsCount = buttons.length;
+							//updateProgressMessage();
+							waitingEndTime = time + 5000;
+							buttons.forEach(function(button) {
+								if (!button.classList.contains('is-following'))
+									return;
+								
+								if (!config.imitate) button.click();
+								else {button.classList.remove('is-following'); button.innerHTML = '';}
+								
+								++processedItemsCount;
+								updateProgressMessage();
+							});
+							document.scrollingElement.scrollTop = document.scrollingElement.scrollHeight;
+							return false;
+						}
+						if (document.querySelector('.loader')) {
+							waitingEndTime = -Infinity;
+							return false;
+						}
+						if (time <= waitingEndTime)
+							return false;
+						ourForm.setProgressMessage('Finished after unfollowing ' + processedItemsCount + ' items.');
+						ourForm.finished();
+					});
+				};
+				ourForm.onAbort = function() {
+					if (loopingId)
+						clearTimeout(loopingId);
+					ourForm.setProgressMessage('Cancelled after unfollowing ' + processedItemsCount + ' items!');
+				};
+			}
+			else {
+				var processedItemsCount;
+				function updateProgressMessage() {ourForm.setProgressMessage(processedItemsCount + ' were unfollowed so far...');}
+				var currentHeight;
+				var unfollowingId;
+				var scrollingId;
+				var endCheckingId;
+				
+				ourForm.onStart = function() {
+					processedItemsCount = 0;
+					updateProgressMessage();
+
+					currentHeight = document.body.scrollHeight;
+					unfollowingId = setInterval(function() {
+						var buttons = getFollowUnfollowButtons();
+						for(var b = buttons.length - 1; b >= 0; --b) {
+							if (!buttons[b].classList.contains('is-following'))
+								continue;
+							
+							if (!config.imitate) buttons[b].click();
+							else {buttons[b].classList.remove('is-following'); buttons[b].innerHTML = '';}
+							
+							++processedItemsCount;
+							updateProgressMessage();
+						}
+					}, 1000);
+					scrollingId = setInterval(function() {scrollBy(0, 12.5);}, 15);
+					endCheckingId = setInterval(function() {
+						if (document.body.scrollHeight <= currentHeight) {
+							clearInterval(endCheckingId);
+							clearInterval(scrollingId);
+							clearInterval(unfollowingId);
+							ourForm.setProgressMessage('Finished after unfollowing ' + processedItemsCount + ' items.');
+							ourForm.finished();
+							return;
+						}
+						currentHeight = document.body.scrollHeight;
+					}, 7.5 * 1000);
+				};
+				ourForm.onAbort = function() {
+					clearInterval(endCheckingId);
+					clearInterval(scrollingId);
+					clearInterval(unfollowingId);
+					ourForm.setProgressMessage('Cancelled after unfollowing ' + processedItemsCount + ' items!');
+				};
+			}
+			ourForm.show();
+		});
+	};
+
+	window.ignoreInvitations = function() {
+		doWhenLoaded(function() {
+			var ourForm = new OurForm('Are You Sure You Want to Ignore Every Invitation On This Page?');
+			{
+				var processedItems;
+				var loopingId;
+				ourForm.onStart = function() {
+					processedItems = new Set();
+					loopingId = retryWhileFails(function() {
+						var button = document.querySelector('.mn-invitation-card [data-control-name="decline"]');
+						if (button) {
+							if (!processedItems.has(button)) {
+								if (!config.imitate) button.click();
+								else {var li = button.closest('.mn-invitation-card'); li.parentNode.removeChild(li);}
+								processedItems.add(button);
+								ourForm.setProgressMessage('Ignored ' + processedItems.size + ' invitations so far...');
+							}
+							return false;
+						}
+						ourForm.setProgressMessage('Finished after ignoring ' + processedItems.size + ' invitations.');
+						ourForm.finished();
+					});
+				};
+				ourForm.onAbort = function() {
+					if (loopingId)
+						clearTimeout(loopingId);
+					ourForm.setProgressMessage('Cancelled after ignoring ' + processedItems.size + ' invitations!');
+				};
+			}
+			ourForm.show();
+		});
+	};
+
+	window.withdraw = function() {
+		doWhenLoaded(function() {
+			var ourForm = new OurForm('Are You Sure You Want to Withdraw Every Invitation On This Page?');
+			if (config.withdrawMinimalPause !== false) {
+				var processedItems;
+				var loopingId;
+				ourForm.onStart = function() {
+					processedItems = new Set();
+					loopingId = retryWhileFails(function() {
+						var button = document.querySelector('.mn-person-card [data-control-name="withdraw_single"]');
+						if (button) {
+							if (!processedItems.has(button)) {
+								if (!config.imitate) button.click();
+								else {var li = button.closest('.mn-person-card'); li.parentNode.removeChild(li);}
+								processedItems.add(button);
+								ourForm.setProgressMessage('Withdrawed ' + processedItems.size + ' invitations so far...');
+								return config.withdrawMinimalPause;
+							}
+							return false;
+						}
+						ourForm.setProgressMessage('Finished after withdrawing ' + processedItems.size + ' invitations.');
+						ourForm.finished();
+					});
+				};
+				ourForm.onAbort = function() {
+					if (loopingId)
+						clearTimeout(loopingId);
+					ourForm.setProgressMessage('Cancelled after withdrawing ' + processedItems.size + ' invitations!');
+				};
+			}
+			else {
+				var processedItems;
+				var loopingId;
+				ourForm.onStart = function() {
+					processedItems = new Set();
+					loopingId = retryWhileFails(function() {
+						var buttons = document.querySelectorAll('.mn-person-card [data-control-name="withdraw_single"]');
+						if (buttons.length != 0) {
+							buttons.forEach(function(button) {
+								if (processedItems.has(button))
+									return;
+								button.click();
+								processedItems.add(button);
+								ourForm.setProgressMessage('Withdrawed ' + processedItems.size + ' invitations so far...');
+							});
+							return false;
+						}
+						ourForm.setProgressMessage('Finished after withdrawing ' + processedItems.size + ' invitations.');
+						ourForm.finished();
+					});
+				};
+				ourForm.onAbort = function() {
+					if (loopingId)
+						clearTimeout(loopingId);
+					ourForm.setProgressMessage('Cancelled after withdrawing ' + processedItems.size + ' invitations!');
+				};
+			}
+			ourForm.show();
+		});
+	};
+})();
